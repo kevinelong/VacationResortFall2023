c9e2bc393556b1130f98e2dcff083e0ca4c0810d
kevinelong <kevinelong@gmail.com>
2023-10-27 13:08:44 -0700

    nav and styles

---
+++ scripts/nav.js
@@ -0,0 +1,17 @@
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+
+
+        header.innerHTML = `
+            <nav>
+                <a href="index.html"> HOME </a>
+                <a href="overnight.html"> Calculator </a>
+                <a href="dining.html"> Dining </a>
+                <a href="car.html"> Car Rental </a>
+                <a href="activities.html"> Activities </a>
+            </nav>
+        `;
+
+    }// END ONLOAD
+);
